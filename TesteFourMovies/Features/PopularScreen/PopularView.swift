a11fbb350a34d0ab313bcc085498761201cb2941
PEDRO HENRIQUE DE OLIVEIRA MORAES <86898701+PedroMoraes89@users.noreply.github.com>
2022-09-28 06:00:04 -0700

    Create Atributtes

--- TesteFourMovies/Features/PopularScreen/PopularView.swift
+++ TesteFourMovies/Features/PopularScreen/PopularView.swift
@@ -11,6 +11,51 @@ class PopularView: UIViewController {
     
         // MARK: - Atributtes
     
+    private let removeCenterBorder: UIView = {
+       let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .black
+        view.isHidden = false
+        return view
+    }()
+    
+    private lazy var UpcomingButton: UIButton = {
+        
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Upcoming", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.backgroundColor = .white
+        button.layer.borderColor = UIColor(cgColor: CGColor(red: 250/255, green: 238/255, blue: 239/255, alpha: 1)).cgColor
+        button.layer.borderWidth = 1
+        button.layer.cornerRadius = 5
+        button.addTarget(self, action: #selector(actionUpComingButton), for: .touchUpInside)
+       return button
+    }()
+    
+    private lazy var popularButton: UIButton = {
+        
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Popular", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = .black
+        button.layer.borderColor = UIColor(cgColor: CGColor(red: 250/255, green: 238/255, blue: 239/255, alpha: 1)).cgColor
+        button.layer.borderWidth = 1
+        button.layer.cornerRadius = 5
+        button.addTarget(self, action: #selector(actionPopularButton), for: .touchUpInside)
+       return button
+    }()
+    
+    private lazy var popularMoviesLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Popular Movies"
+        label.textColor = .white
+        label.textAlignment = .left
+        label.font = UIFont.boldSystemFont(ofSize: 40)
+        return label
+    
     private lazy var movieCollectionView: UICollectionView = {
         let collectionViewLayout = UICollectionViewFlowLayout()
         collectionViewLayout.scrollDirection = .vertical
