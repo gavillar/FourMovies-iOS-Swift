7f85f5429b2bde034a2458400e55db77b03e291e
PEDRO HENRIQUE DE OLIVEIRA MORAES <86898701+PedroMoraes89@users.noreply.github.com>
2022-09-28 05:32:05 -0700

    Create class PopularView and private lazy var movieCollectionView

--- TesteFourMovies/Features/PopularScreen/PopularView.swift
+++ TesteFourMovies/Features/PopularScreen/PopularView.swift
@@ -5,4 +5,27 @@
 //  Created by user220210 on 9/27/22.
 //
 
-import Foundation
+import UIKit
+
+class PopularView: UIViewController {
+    
+        // MARK: - Atributtes
+    
+    private lazy var movieCollectionView: UICollectionView = {
+        let collectionViewLayout = UICollectionViewFlowLayout()
+        collectionViewLayout.scrollDirection = .vertical
+        collectionViewLayout.itemSize = CGSize(width: 100, height: 150)
+        collectionViewLayout.sectionInset = UIEdgeInsets(top: 0, left: 20, bottom: 20, right: 20)
+        
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: collectionViewLayout)
+        collectionView.showsHorizontalScrollIndicator = false
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        return collectionView
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // MARK: - Functions
+    }
+}
