600ebfde71bdbd71f8adaedbcd646825e419ff7a
VillarDeveloper <villar.developer@gmail.com>
2022-09-28 09:31:09 -0700

    collection view

---
+++ TesteFourMovies/Features/UpcomingScreen/UpcomingViewModel.swift
@@ -0,0 +1,21 @@
+//
+//  UpcomingViewModel.swift
+//  TesteFourMovies
+//
+//  Created by user220831 on 9/28/22.
+//
+
+import Foundation
+
+class UpcomingViewModel {
+    
+    
+    init() {
+      
+        
+    }
+    
+    
+    
+    
+}
