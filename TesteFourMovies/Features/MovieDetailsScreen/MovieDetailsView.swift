6c40e6138470e39519136030083e2b9319d320a1
helogomes <105743745+helogomes@users.noreply.github.com>
2022-09-29 08:01:52 -0700

    scrollview

--- TesteFourMovies/Features/MovieDetailsScreen/MovieDetailsView.swift
+++ TesteFourMovies/Features/MovieDetailsScreen/MovieDetailsView.swift
@@ -11,7 +11,7 @@ class MovieDetailsView: UIViewController{
     
     private var collectionView: UICollectionView?
     
-    let bannerView: UIImageView = {
+    private lazy var bannerView: UIImageView = {
         let image = UIImage(named: "")
         let banner = UIImageView(image: image)
         banner.frame = CGRect(x: 0, y: 0, width: 199, height: 50)
@@ -21,7 +21,7 @@ class MovieDetailsView: UIViewController{
         return banner
     }()
     
-    let titleMovie: UILabel = {
+    private lazy var titleMovie: UILabel = {
         let title = UILabel()
         title.translatesAutoresizingMaskIntoConstraints = false
         title.text = "Teste"
@@ -30,7 +30,7 @@ class MovieDetailsView: UIViewController{
         return title
     }()
     
-    let yearMovie: UILabel = {
+    private lazy var yearMovie: UILabel = {
         let year = UILabel()
         year.translatesAutoresizingMaskIntoConstraints = false
         year.text = "2000"
@@ -39,7 +39,7 @@ class MovieDetailsView: UIViewController{
         return year
     }()
     
-    let characteristicsMovie: UILabel = {
+    private lazy var characteristicsMovie: UILabel = {
         let characteristics = UILabel()
         characteristics.translatesAutoresizingMaskIntoConstraints = false
         characteristics.text = "109m | Animation, Adventure, Comedy"
@@ -53,6 +53,21 @@ class MovieDetailsView: UIViewController{
         view.translatesAutoresizingMaskIntoConstraints = false
         view.backgroundColor = UIColor(red: 0.098, green: 0.106, blue: 0.114, alpha: 1)
         return view
+    }()
+    
+    private lazy var viewInScroll: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = UIColor(red: 0.098, green: 0.106, blue: 0.114, alpha: 1)
+        
+        return view
+    }()
+    
+    private lazy var scrollView: UIScrollView = {
+        let scroll = UIScrollView()
+        scroll.translatesAutoresizingMaskIntoConstraints = false
+        
+        return scroll
     }()
 
     private func castCollection() {
@@ -75,9 +90,9 @@ class MovieDetailsView: UIViewController{
         collectionView.translatesAutoresizingMaskIntoConstraints = false
         backgroundView.addSubview(collectionView)
         
-        collectionView.topAnchor.constraint(equalTo: backgroundView.topAnchor, constant: 20).isActive = true
-        collectionView.leadingAnchor.constraint(equalTo: backgroundView.leadingAnchor, constant: 20).isActive = true
-        collectionView.trailingAnchor.constraint(equalTo: backgroundView.trailingAnchor, constant: -20).isActive = true
+        collectionView.topAnchor.constraint(equalTo: characteristicsMovie.bottomAnchor, constant: 15).isActive = true
+        collectionView.leadingAnchor.constraint(equalTo: backgroundView.leadingAnchor, constant: 15).isActive = true
+        collectionView.trailingAnchor.constraint(equalTo: backgroundView.trailingAnchor, constant: -15).isActive = true
         collectionView.heightAnchor.constraint(equalToConstant: 200).isActive = true
         
         collectionView.delegate = self
@@ -85,20 +100,35 @@ class MovieDetailsView: UIViewController{
         
     }
     
-    let synopsisLabel: UILabel = {
+    private lazy var line: UIView = {
+        
+        let createLine = {() -> UIView in
+            
+            let line = UIView()
+            line.backgroundColor = .darkGray
+            line.translatesAutoresizingMaskIntoConstraints = false
+            return line
+        }
+        
+        let line: UIView = createLine()
+        
+        return line
+    }()
+    
+    private lazy var synopsisLabel: UILabel = {
         let synopsis = UILabel()
         synopsis.translatesAutoresizingMaskIntoConstraints = false
-        synopsis.text = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+        synopsis.text = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
         synopsis.textColor = .white
         synopsis.numberOfLines = 0
-        synopsis.adjustsFontSizeToFitWidth = true
-        synopsis.font = UIFont(name: "Arial", size: 14)
+        synopsis.sizeToFit()
+        synopsis.font = UIFont(name: "Arial", size: 15)
         return synopsis
     }()
     
     override func loadView() {
         super.loadView()
-        self.view.backgroundColor = .black
+        self.view.backgroundColor = UIColor(red: 0.098, green: 0.106, blue: 0.114, alpha: 1)
         addSubViews()
         setConstraints()
         castCollection()
@@ -107,43 +137,63 @@ class MovieDetailsView: UIViewController{
     
     /// This function handles the display of view elements
     private func addSubViews() {
-        view.addSubview(bannerView)
-        view.addSubview(titleMovie)
-        view.addSubview(yearMovie)
-        view.addSubview(characteristicsMovie)
-        view.addSubview(backgroundView)
+        view.addSubview(scrollView)
+        scrollView.addSubview(backgroundView)
+        backgroundView.addSubview(bannerView)
+        backgroundView.addSubview(titleMovie)
+        backgroundView.addSubview(yearMovie)
+        backgroundView.addSubview(characteristicsMovie)
+        backgroundView.addSubview(line)
+        backgroundView.addSubview(viewInScroll)
         backgroundView.addSubview(synopsisLabel)
+        
     }
     
     /// This function handles the constraints of view elements
     private func setConstraints() {
         
         NSLayoutConstraint.activate([
-        
-            bannerView.topAnchor.constraint(equalTo: view.topAnchor, constant: CGFloat(0)),
-            bannerView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: CGFloat(0)),
-            bannerView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: CGFloat(0)),
-            bannerView.heightAnchor.constraint(equalToConstant: CGFloat(300)),
             
-            titleMovie.topAnchor.constraint(equalTo: bannerView.bottomAnchor, constant: CGFloat(20)),
-            titleMovie.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: CGFloat(15)),
-            titleMovie.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: CGFloat(-300)),
+            scrollView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            scrollView.widthAnchor.constraint(equalTo: view.widthAnchor),
+            scrollView.topAnchor.constraint(equalTo: view.topAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+
+            backgroundView.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor),
+            backgroundView.widthAnchor.constraint(equalTo: scrollView.widthAnchor),
+            backgroundView.topAnchor.constraint(equalTo: scrollView.topAnchor),
+            backgroundView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
+        
+            bannerView.topAnchor.constraint(equalTo: backgroundView.topAnchor),
+            bannerView.leadingAnchor.constraint(equalTo: backgroundView.leadingAnchor, constant: CGFloat(0)),
+            bannerView.trailingAnchor.constraint(equalTo: backgroundView.trailingAnchor, constant: CGFloat(0)),
+            bannerView.heightAnchor.constraint(equalToConstant: 300),
+            
+            titleMovie.topAnchor.constraint(equalTo: bannerView.bottomAnchor, constant: 20),
+            titleMovie.leadingAnchor.constraint(equalTo: backgroundView.leadingAnchor, constant: 15),
+            titleMovie.trailingAnchor.constraint(equalTo: backgroundView.trailingAnchor, constant: -300),
             
             yearMovie.topAnchor.constraint(equalTo: bannerView.bottomAnchor, constant: 22),
             yearMovie.leadingAnchor.constraint(equalTo: titleMovie.trailingAnchor, constant: 10),
             
             characteristicsMovie.topAnchor.constraint(equalTo: titleMovie.bottomAnchor, constant: 15),
-            characteristicsMovie.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 15),
-            characteristicsMovie.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            characteristicsMovie.leadingAnchor.constraint(equalTo: backgroundView.leadingAnchor, constant: 15),
+            characteristicsMovie.trailingAnchor.constraint(equalTo: backgroundView.trailingAnchor, constant: -20),
             
-            backgroundView.topAnchor.constraint(equalTo: characteristicsMovie.bottomAnchor, constant: 20),
-            backgroundView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            backgroundView.widthAnchor.constraint(equalTo: view.widthAnchor),
-            backgroundView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            line.topAnchor.constraint(equalTo: characteristicsMovie.bottomAnchor, constant: 230),
+            line.trailingAnchor.constraint(equalTo: backgroundView.trailingAnchor, constant: -15),
+            line.leadingAnchor.constraint(equalTo: backgroundView.leadingAnchor, constant: 15),
+            line.heightAnchor.constraint(equalToConstant: 0.5),
             
-            synopsisLabel.topAnchor.constraint(equalTo: backgroundView.topAnchor, constant: 250),
-            synopsisLabel.leadingAnchor.constraint(equalTo: backgroundView.leadingAnchor, constant: 20),
-            synopsisLabel.trailingAnchor.constraint(equalTo: backgroundView.trailingAnchor, constant: -20)
+            viewInScroll.topAnchor.constraint(equalTo: line.bottomAnchor),
+            viewInScroll.leadingAnchor.constraint(equalTo: backgroundView.leadingAnchor),
+            viewInScroll.trailingAnchor.constraint(equalTo: backgroundView.trailingAnchor),
+            viewInScroll.bottomAnchor.constraint(equalTo: backgroundView.bottomAnchor),
+            
+            synopsisLabel.topAnchor.constraint(equalTo: line.bottomAnchor, constant: 15),
+            synopsisLabel.leadingAnchor.constraint(equalTo: viewInScroll.leadingAnchor, constant: 15),
+            synopsisLabel.trailingAnchor.constraint(equalTo: viewInScroll.trailingAnchor, constant: -15),
+            synopsisLabel.bottomAnchor.constraint(equalTo: viewInScroll.bottomAnchor, constant: -20),
         
             
            
@@ -168,3 +218,4 @@ extension MovieDetailsView: UICollectionViewDelegate, UICollectionViewDataSource
     
     
 }
+
